f809b485834595b939b6f0b4305523f1c0bb95f2
FarahNajib <farahengnajib@gmail.com>
2021-01-27 22:48:20 +0800

    tbd

--- jsstyle.js
+++
@@ -1,9 +0,0 @@
-var selected = 'js-is-selected';
-
-$('.favorite').click(function() {
-  if( $(this).hasClass(selected)) {
-    $(this).removeClass(selected)
-  } else {
-    $(this).addClass(selected)
-  }
-})
